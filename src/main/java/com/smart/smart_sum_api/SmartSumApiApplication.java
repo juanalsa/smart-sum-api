c6fd902c995a142bd53a373cad8c805729c5d993
Julian Alvarado Sanchez <juanalsa@gmail.com>
2025-04-30 22:59:55 -0500

    :wrench: First commit

---
+++ src/main/java/com/smart/smart_sum_api/SmartSumApiApplication.java
@@ -0,0 +1,13 @@
+package com.smart.smart_sum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart/smart_sum_api/SmartSumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.smart_sum_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
